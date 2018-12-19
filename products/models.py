87192df5341b204abdb246b43e69f7b8746aa116
leihua <leihua@masslottery.com>
2018-12-19 13:32:35 -0500

    model product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=None, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=29.99)
+    slug = models.SlugField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
