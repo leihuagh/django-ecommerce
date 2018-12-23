f48f3d2ccb63602cb488aaccb0ce1b0d1558506e
leihua <leihua@masslottery.com>
2018-12-22 23:53:39 -0500

    customize admin

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,14 @@ from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
     search_fields = ['title', 'description', 'price']
     list_display = ['__str__', 'description',
-                    'price', 'slug', 'created', 'updated']
+                    'price', 'slug', 'active', 'created', 'updated']
+    list_editable = ['price', 'active']
+    list_filter = ['price', 'active']
+    readonly_fields = ['created', 'updated']
+    prepopulated_fields = {"slug": ("title",)}
 
     class Meta:
         model = Product
