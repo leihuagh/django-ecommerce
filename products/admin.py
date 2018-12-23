2a7f4f776df27fec00453480d4623c04bda9981a
leihua <leihua@masslottery.com>
2018-12-22 21:30:29 -0500

    customize admin product list

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price', 'slug', 'created', 'updated']
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
