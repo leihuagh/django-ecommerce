ae0c48fa4bc8829e6093ce56f7edfaa72b2da3a4
leihua <leihua@masslottery.com>
2018-12-24 18:48:53 -0500

    add sale price to Product model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,8 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=29.99)
+    sale_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
     slug = models.SlugField(max_length=50)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
