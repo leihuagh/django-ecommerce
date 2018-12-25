5194e38e51353d1f1d5375e30ac885bcfa269619
leihua <leihua@masslottery.com>
2018-12-25 18:08:05 -0500

    register ProductImage

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -17,3 +17,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+admin.site.register(ProductImage)
--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
         return self.price
 
 
-class ProductIamge(models.Model):
+class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/images/')
     featured = models.BooleanField(default=False)
